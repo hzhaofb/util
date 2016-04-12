a46e73ace6cb8663f2665033f2e25c7b6fb62eea
Howard Zhao <hzhao@flipboard.com>
2016-04-12 16:48:38 -0700

    use iter and map

--- hbase/merge_region.py
+++ hbase/merge_region.py
@@ -19,10 +19,16 @@ def hbexec(cmd):
     p = Popen(["hbase", "shell"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     return p.communicate(input=cmd)[0]
 
+def merge(r1, r2):
+    if r1 and r2:
+        hbexec("merge_region '%s', '%s'" % (r1, r2))
+        # wait for some time for hbase to catch up
+        print "sleep 20"
+        time.sleep(20)
+
 rs = hbexec("scan 'hbase:meta',{FILTER=>\"PrefixFilter('%s')\", COLUMNS => 'info:regioninfo'}" % sys.argv[1])
+# extract regionid
 rs = map(regionid, rs.split("\n"))
 rs = filter(lambda x:x, rs)
-for i in range((len(rs) - 1)/2) :
-    hbexec("merge_region '%s', '%s'" % (rs[i*2],rs[i*2+1]))
-    print "sleep 20 seconds"
-    time.sleep(20)
+# group ever 2 region ids and merge them
+map(merge, *([iter(rs)] *2))
